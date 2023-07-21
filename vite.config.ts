2c5ea45b12b8691442be999696b00282c489cef2
Nuku Malik Sanjaya Kusuma <nukumalik@gmail.com>
2023-07-21 08:46:58 +0700

    bugfix: add base value on vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react-github-search/',
   test: {
     globals: true,
     environment: 'jsdom',
